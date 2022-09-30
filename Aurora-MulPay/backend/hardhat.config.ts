a5b48d8765666753534ed8c2f73bf4dbff0b255e
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-09-30 16:31:51 +0900

    下準備

--- Aurora-MulPay/backend/hardhat.config.ts
+++ Aurora-MulPay/backend/hardhat.config.ts
@@ -1,8 +1,24 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@typechain/hardhat";
+require("dotenv").config();
 
+const {
+  AURORA_PRIVATE_KEY
+} = process.env;
+
+/**
+ * hardhat config 
+ */
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: "0.8.0",
+  networks: {
+    // aurora testnet
+    testnet_aurora: {
+      url: "https://testnet.aurora.dev",
+      accounts: [AURORA_PRIVATE_KEY!],
+    },
+  },
 };
 
 export default config;
