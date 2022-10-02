835a00fcd0733f9bb607694f07d60bc74e1948ff
mashharuki <avp.104.106.107@ae.auone-net.jp>
2022-10-02 15:11:01 +0900

    coinとnavbarコンポーネントを追加しました。

---
+++ Aurora-MulPay/frontend/lib/view/widgets/coin.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+/**
+ * Coins widget
+ */
+Widget Coins(
+    double displayWidth, 
+    displayHeight, 
+    String imagePath, 
+    symbol, 
+    name,
+    depo, 
+    value
+) {
+
+    return Container(
+        height: displayHeight * 0.08,
+        width: displayWidth,
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        margin: EdgeInsets.symmetric(vertical: displayHeight * 0.008),
+        decoration: BoxDecoration(
+            borderRadius: const BorderRadius.all(
+                Radius.circular(20),
+            ),
+            color: HexColor('#054C74'),
+        ),
+        child: Row(
+            children: [
+                SizedBox(
+                    height: 37,
+                    width: 37,
+                    child: Image.asset(imagePath),
+                ),
+                const SizedBox(width: 13,),
+                Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                        Text(
+                            symbol,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontSize: 14,
+                            ),
+                        ),
+                        Text(
+                            name,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey,
+                                fontSize: 12
+                            ),
+                        ),
+                    ],
+                ),
+            ],
+        ),
+        Expanded(
+            child: SizedBox(),
+        ),
+        SizedBox(
+            width: displayWidth * 0.18,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                    Text(
+                        '${depo} ${symbol}',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 13,
+                        ),
+                    ),
+                    Text(
+                        '${value} ETH',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 12
+                        ),
+                    ),
+                ],
+            ),
+        ),
+    );
+}
---
+++ Aurora-MulPay/frontend/lib/view/widgets/navbar.dart
@@ -0,0 +1,86 @@
+import 'package:fluid_bottom_nav_bar/fluid_bottom_nav_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:mulpay_frontend/view/screens/home.dart';
+import 'package:mulpay_frontend/view/screens/send.dart';
+import 'package:mulpay_frontend/view/screens/wallet.dart';
+import 'package:provider/provider.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+/**
+ * BottomNavigationBarProvider
+ */
+class BottomNavigationBarProvider with ChangeNotifier {
+    int _currentIndex = 0;
+
+    int get currentIndex => _currentIndex;
+
+    set currentIndex(int index) {
+        _currentIndex = index;
+        notifyListeners();
+    }
+}
+
+/**
+ * BottomNavigationBarWidget
+ */
+class BottomNavigationBarWidget extends StatefulWidget {
+    const BottomNavigationBarWidget({Key? key}) : super(key: key);
+
+    @override
+    State<BottomNavigationBarWidget> createState() => _BottomNavigationBarState();
+}
+
+/**
+ * _BottomNavigationBarState
+ */
+class _BottomNavigationBarState extends State<BottomNavigationBarWidget> {
+
+    var currentTab = [
+        const Home(),
+        const Send(),
+        const Wallet(),
+    ];
+
+    @override
+    Widget build(BuildContext context) {
+        var provider = Provider.of<BottomNavigationBarProvider>(context);
+        
+        return Scaffold(
+            body: currentTab[provider.currentIndex],
+            bottomNavigationBar: FluidNavBar(
+                icons: [
+                    FluidNavBarIcon(
+                        svgPath: "assets/home.svg",
+                        extras: {"label": "home"},
+                        backgroundColor: provider.currentIndex == 0 ? HexColor("#0C9DB0") : Colors.black,
+                    ),
+                    FluidNavBarIcon(
+                        svgPath: "assets/pay.svg",
+                        extras: {"label": "send"},
+                        backgroundColor: provider.currentIndex == 1 ? HexColor("#0C9DB0") : Colors.black,
+                    ),
+                    FluidNavBarIcon(
+                        svgPath: "assets/wallet.svg",
+                        extras: {"label": "wallet"},
+                        backgroundColor: provider.currentIndex == 2 ? HexColor("#0C9DB0") : Colors.black,
+                    )
+                ],
+                onChange: (index) => {
+                    provider.currentIndex = index,
+                },
+                style: const FluidNavBarStyle(
+                    iconUnselectedForegroundColor: Colors.grey,
+                    iconSelectedForegroundColor: Colors.white,
+                    barBackgroundColor: Colors.black,
+                ),
+                scaleFactor: 5,
+                defaultIndex: 0,
+                itemBuilder: (icon, item) => Semantics(
+                    label: icon.extras!["label"],
+                    child: item,
+                ),
+                animationFactor: 0.7,
+            ),
+        );
+    }
+}
